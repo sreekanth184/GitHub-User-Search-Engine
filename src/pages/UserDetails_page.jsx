b7a355032a0940b970196fcdc3b1d420303c2dfe
sreekanth <sreekanthreddy184@gmail.com>
2024-10-22 15:09:16 +0530

    added new features

--- src/pages/UserDetails_page.jsx
+++ src/pages/UserDetails_page.jsx
@@ -36,7 +36,7 @@ function UserDetails_page({ isLoading, setIsLoading, isError, setIsError }) {
 
   if (isLoading) {
     return (
-      <div className="flex-col gap-4 w-full flex items-center justify-center">
+      <div className="flex-col gap-4 w-full flex items-center justify-center mt-20">
         <div className="w-20 h-20 border-4 border-transparent text-blue-600 text-4xl animate-spin flex items-center justify-center border-t-blue-600 rounded-full">
           <div className="w-16 h-16 border-4 border-transparent text-purple-600 text-2xl animate-spin flex items-center justify-center border-t-purple-600 rounded-full"></div>
         </div>
